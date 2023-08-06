4de48b4ba2fd135a71f0ac19d946ad4865fe2f06
shahrukhjaved786 <shahrukh.tjit14@gmail.com>
2023-08-06 23:54:59 +0530

    updated sample csv file path in code

--- PD01_S10.1_1_CSV-ReadFile.py
+++ PD01_S10.1_1_CSV-ReadFile.py
@@ -1,6 +1,6 @@
 import csv
 
-with open(r"/Python/CSV/csvFile_userdata.csv", "r") as file_data:
+with open(r"H:\Pycharm_Projects\pythonProject\PracticeProject\Python\CSV_File_Operations_with_Python\csvFile_userdata.csv", "r") as file_data:
     csv_obj = csv.reader(file_data,delimiter=",")
     #print(list(csv_obj))
 
--- PD01_S10.1_2_CSV-ReadFile-Validation.py
+++ PD01_S10.1_2_CSV-ReadFile-Validation.py
@@ -4,7 +4,7 @@ import csv
 
 username = input("Enter The Name to Get Loan Status: ")
 
-with open(r"/Python/CSV/csvFile_userdata.csv", "r") as file_data:
+with open(r"H:\Pycharm_Projects\pythonProject\PracticeProject\Python\CSV_File_Operations_with_Python\csvFile_userdata.csv", "r") as file_data:
     csv_obj = csv.reader(file_data,delimiter=',')
     #print(list(csv_obj))
     list_name = []
@@ -14,15 +14,16 @@ with open(r"/Python/CSV/csvFile_userdata.csv", "r") as file_data:
         list_name.append(value[0])
         list_status.append(value[1])
 
-    # for name,status in list(csv_obj):
-    #     list_name.append(name)
-    #     list_status.append(status)
 
-for indexpos , name in enumerate(list_name):
-    if name == username:
-        print(f"Loan Status of {username} is {list_status[indexpos]}")
+    for name,status in list(csv_obj):
+        list_name.append(name)
+        list_status.append(status)
+
+# for indexpos , name in enumerate(list_name):
+#     if name == username:
+#         print(f"Loan Status of {username} is {list_status[indexpos]}")
 
 
-# indexpos = list_name.index(username)
-# loanstatus = list_status[indexpos]
-# print(f"Loan status of {username} is {loanstatus}" )
+indexpos = list_name.index(username)
+loanstatus = list_status[indexpos]
+print(f"Loan status of {username} is {loanstatus}" )
