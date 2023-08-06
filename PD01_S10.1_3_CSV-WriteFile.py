e159a4a1ac70eb990c7bc3975a62ac2c1a170937
shahrukhjaved786 <shahrukh.tjit14@gmail.com>
2023-08-06 23:57:28 +0530

    updated sample file path in code

--- PD01_S10.1_3_CSV-WriteFile.py
+++ PD01_S10.1_3_CSV-WriteFile.py
@@ -1,6 +1,6 @@
 import csv
 
-with open(r"/Python/CSV/csvFile_userdata.csv", "a") as file_data:
+with open(r"H:\Pycharm_Projects\pythonProject\PracticeProject\Python\CSV_File_Operations_with_Python\csvFile_userdata.csv", "a") as file_data:
     csv_obj = csv.writer(file_data)
-    csv_obj.writerow(["Dorsy","Rejected"])
+    csv_obj.writerow(["Angy","Pending"])
 
